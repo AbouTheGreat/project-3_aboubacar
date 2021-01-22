1bf20ab3770dac57cb6645e5bf44550857dcf496
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2021-01-21 21:22:26 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 while True:
+    light.set_all(color.rgb(0, 255, 0))
     print ("light level: " + input.light_level() + " Sound level: " + input.sound_level())
-    if input.sound_level() > 100 + input.light_level() > 5:
-        print("Loud noise detected, keep you're lights off to stay safe!")
-        light.set_brightness(1000)
+    if input.light_level() >= 9:
+        print("Your lights are on, to stay safe turn them off")
+        light.set_all(color.rgb(255, 100, 0))
+        pause(500)
+        light.clear()
+        pause(500)
+    if input.sound_level() > 150:
+        print("A loud noise just occurred, stay alert!")
         light.set_all(color.rgb(255, 0, 0))
         pause(500)
         light.clear()
         pause(500)
-        light.set_all(color.rgb(255, 0, 0))
-        pause(500)
-        light.set_all(color.rgb(255, 0, 0))
-    if input.sound_level() > 100 :
-        print("There was a loud noise! Your lights are already off, make sure you're safe!")
-        light.set_brightness(1000)
-        light.set_all(color.rgb(255, 0, 0))
-        pause(500)
-        light.clear()
-        pause(500)
-        light.set_all(color.rgb(255, 0, 0))
-        pause(500)
-        light.set_all(color.rgb(255, 0, 0))
     
-    
-           
-           
+       
--- main.ts
+++ main.ts
@@ -1,27 +1,20 @@
 while (true) {
+    light.setAll(color.rgb(0, 255, 0))
     console.log("light level: " + input.lightLevel() + " Sound level: " + input.soundLevel())
-    if (input.soundLevel() > 100 + input.lightLevel() && 100 + input.lightLevel() > 5) {
-        console.log("Loud noise detected, keep you're lights off to stay safe!")
-        light.setBrightness(1000)
-        light.setAll(color.rgb(255, 0, 0))
+    if (input.lightLevel() >= 9) {
+        console.log("Your lights are on, to stay safe turn them off")
+        light.setAll(color.rgb(255, 100, 0))
         pause(500)
         light.clear()
         pause(500)
-        light.setAll(color.rgb(255, 0, 0))
-        pause(500)
-        light.setAll(color.rgb(255, 0, 0))
     }
     
-    if (input.soundLevel() > 100) {
-        console.log("There was a loud noise! Your lights are already off, make sure you're safe!")
-        light.setBrightness(1000)
+    if (input.soundLevel() > 150) {
+        console.log("A loud noise just occurred, stay alert!")
         light.setAll(color.rgb(255, 0, 0))
         pause(500)
         light.clear()
         pause(500)
-        light.setAll(color.rgb(255, 0, 0))
-        pause(500)
-        light.setAll(color.rgb(255, 0, 0))
     }
     
 }
